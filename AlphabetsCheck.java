de1fff979405017f6e3bfdb08331359639e84633
kasalaManishkumar <98003508+kasalaManishkumar@users.noreply.github.com>
2022-02-08 10:36:45 +0530

    Add files via upload

---
+++ AlphabetsCheck.java
@@ -0,0 +1,23 @@
+package letters;
+
+import java.util.Scanner;
+
+public class AlphabetsCheck {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter a string");
+        String input=sc.next();
+        boolean check=false;
+        for(int i=0;i<input.length();i++){
+            if(!(input.charAt(i)>='a' && input.charAt(i)<='z' ||input.charAt(i)>='A' && input.charAt(i)<='Z')) {
+                check = true;
+            }
+        }
+        if(check){
+            System.out.println("It also contains other than a-z && A-Z");
+        }
+        else{
+            System.out.println("all the alphabets are a-z && A-Z");
+        }
+    }
+}
